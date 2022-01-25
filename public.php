c55f40e36ed90879e6b24fea02c00658eea3a7ce
thanghoo-211 <huuthang911@gmail.com>
2022-01-25 23:19:59 +0700

    add function php

--- public.php
+++ public.php
@@ -2,6 +2,9 @@
 include("./utils/db.php");
 function getProductByName($name) {
   $conn = newConnection();
-  $query = "";
+  $query = "SELECT * FROM `product` WHERE `result` = '$name';
+  $result = $conn->query($query);
+  $row = $result->fetch_array(MYSQLI_BOTH);
+  
 }
 ?>
